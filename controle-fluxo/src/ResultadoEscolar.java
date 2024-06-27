0a80a8206b5222c41400833a6a80a046da6477f7
zaghard <mauricioalmeida.info@gmail.com>
2024-06-27 17:35:06 -0300

    Estudos de operadores ternarios

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+// ResultadoEscolar.java
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        int nota = 10;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7)// true or false;
+            System.out.println("Prova de Recuperação");
+
+        else
+            System.out.println("Reprovado");
+    }
+}
